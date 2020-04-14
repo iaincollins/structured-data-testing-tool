a173bb371323345ac6aacc96b54ab0119d0348e2
Iain Collins <me@iaincollins.com>
2020-04-14 15:50:31 +0100

    Refactor to move code out from index.js

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const WAE = require('web-auto-extractor').default
 const fetch = require('node-fetch')
-const jmespath = require('jmespath')
 const validator = require('validator')
 const isStream = require('is-stream')
 const getStream = require('get-stream')
+
+const { runTest } = require('./lib/tests')
 const { schemas: validSchemas, getSchema } = require('./lib/schemas')
+const { getTestsFromPreset } = require('./lib/helpers/presets')
+const {
+  isSchemaOrgSchema,
+  getSchemaTypeAndName,
+  addTestsForSchemaOrgSchema,
+  findSchemasInStructuredData
+} = require('./lib/helpers/schemas')
 
 const _structuredDataTest = async (structuredData, options) => {
   try {
@@ -16,7 +24,7 @@ const _structuredDataTest = async (structuredData, options) => {
 
     const presetsSpecified = (options && options.presets) ? options.presets : []
     const schemasSpecified = (options && options.schemas) ? options.schemas : []
-    const schemasFound = _findSchemas(structuredData) // Find schemas in structuredData
+    const schemasFound = findSchemasInStructuredData(structuredData) // Find schemas in structuredData
     const schemas = [] // All schemas found *or* explicitly specified
     let tests = options.tests || [] // Contains all tests to be run
     let testGroups = []
@@ -34,7 +42,7 @@ const _structuredDataTest = async (structuredData, options) => {
       // (jsonld, microdata, rdfa, etc) so can easily add the specific tests for them.
       arrayOfSchemas = schemasFound.concat(
         schemasSpecified.filter(schemaSpecified => {
-          let [ structuredDataType, schemaName ] = _getSchemaTypeAndName(schemaSpecified)
+          let [ structuredDataType, schemaName ] = getSchemaTypeAndName(schemaSpecified)
           if (!schemaName)
             schemaName = structuredDataType
 
@@ -53,7 +61,7 @@ const _structuredDataTest = async (structuredData, options) => {
     const schemasWithProperties = {}
     await Promise.all(
       arrayOfSchemas.map(async schema => {
-        let [ structuredDataType, schemaName ] = _getSchemaTypeAndName(schema)
+        let [ structuredDataType, schemaName ] = getSchemaTypeAndName(schema)
 
         if (!schemaName) {
           schemaName = structuredDataType
@@ -92,7 +100,7 @@ const _structuredDataTest = async (structuredData, options) => {
       // A schema can just be the schema name like 'Article', but it can also be 
       // 'jsonld:Article', 'microdata:Article' or 'rdfa:Article' if you wanted to check
       // for a schema specified on a page in a particular way.
-      let [ structuredDataType, schemaName ] = _getSchemaTypeAndName(schema)
+      let [ structuredDataType, schemaName ] = getSchemaTypeAndName(schema)
 
       // Add schema name to list of schemas
       schemas.push(schemaName)
@@ -221,13 +229,13 @@ const _structuredDataTest = async (structuredData, options) => {
 
           schemaInstances.map((schemaInstance, i) => {
             // Only add Schema.org tests for schemas found that are schema.org schemas or if Schema was explcitly specified to be tested
-            if (_isSchemaOrgSchema(schemaInstance) || schemasSpecified.includes(schemaName)) {
-              _addTestsForSchemaOrgSchema(tests, schemaName, schemaGroups.concat(`#${i} (${structuredDataType})`), structuredDataType, i)
+            if (isSchemaOrgSchema(schemaInstance) || schemasSpecified.includes(schemaName)) {
+              addTestsForSchemaOrgSchema(tests, schemaName, schemaGroups.concat(`#${i} (${structuredDataType})`), structuredDataType, i)
               _addTestsForSchemaOrgProperties(schemaName, schemaGroups.concat(`#${i} (${structuredDataType})`), structuredDataType, schemaInstance, null, i)
             }
           })
         } else {
-          _addTestsForSchemaOrgSchema(tests, schemaName, schemaGroups, structuredDataType)
+          addTestsForSchemaOrgSchema(tests, schemaName, schemaGroups, structuredDataType)
           _addTestsForSchemaOrgProperties(schemaName, schemaGroups.concat(`#0 (${structuredDataType})`), structuredDataType, null, null, 0)
         }
       } else {
@@ -239,7 +247,7 @@ const _structuredDataTest = async (structuredData, options) => {
     schemaTestsWithoutType.forEach(schemaName => {
       if (!schemaTestsWithType.includes(schemaName)) {
         const schemaGroups = (Object(validSchemas).hasOwnProperty(schemaName)) ? ['Schema.org', schemaName] : [schemaName]
-        _addTestsForSchemaOrgSchema(tests, schemaName, schemaGroups)
+        addTestsForSchemaOrgSchema(tests, schemaName, schemaGroups)
       }
     })
 
@@ -261,7 +269,7 @@ const _structuredDataTest = async (structuredData, options) => {
         if (preset.conditional) {
           // Evaluate `preset.conditional` test against structuredData to see if
           // preset tests should be added (don't run if does not pass)
-          const { testPassed } = _runTest(preset.conditional, structuredData)
+          const { testPassed } = runTest(preset.conditional, structuredData)
           if (!testPassed)
             ignorePreset = true
         }
@@ -276,7 +284,7 @@ const _structuredDataTest = async (structuredData, options) => {
             // Evaluate `test.conditional` test against structuredData to see if
             // test should be added (add test to skipped tests if not)
             if (test.conditional) {
-              const { testPassed } = _runTest(test.conditional, structuredData)
+              const { testPassed } = runTest(test.conditional, structuredData)
               if (!testPassed)
                 skipTest = true
             }
@@ -313,7 +321,7 @@ const _structuredDataTest = async (structuredData, options) => {
       if (test.group && !testGroups.includes(test.group))
         testGroups.push(test.group)
 
-      const { testPassed, testError } = _runTest(test, structuredData)
+      const { testPassed, testError } = runTest(test, structuredData)
       test.passed = testPassed
       test.error = testError
 
@@ -342,7 +350,7 @@ const _structuredDataTest = async (structuredData, options) => {
       skipped: testsSkipped,
       groups: testGroups,
       schemas: schemasFound.map(schema => { 
-        let [ type, name ] = _getSchemaTypeAndName(schema)
+        let [ type, name ] = getSchemaTypeAndName(schema)
         return name
       }),
       structuredData,
@@ -362,200 +370,6 @@ const _structuredDataTest = async (structuredData, options) => {
   } catch (e) {
     console.log("Internal error", e)
   }
-}
-
-const _test = (test, json) => {
-  // TEST_DEFAULT_VALUE is useful to be able to set to normalize null/undefined/empty values
-  // This is particularly useful when testing metadata properties which would otherwise have
-  // different default values depending on the reason for a test failing, depending on if the
-  // property is missing, the value is missing or if the value is empty.
-  const TEST_DEFAULT_VALUE = ''
-
-  let testPassed = false
-  let testError = null
-  let path = null
-
-  test.value = TEST_DEFAULT_VALUE
-
-  try {
-    path = test.test
-    const pathValue = jmespath.search(json, path)
-
-    test.value = test.type === 'metatag'
-      ? (pathValue && pathValue[0] && typeof pathValue[0] !== 'undefined') ? pathValue[0] : TEST_DEFAULT_VALUE
-      : (pathValue !== null) ? pathValue : TEST_DEFAULT_VALUE
-
-    if (typeof test.expect === 'undefined' || test.expect === true) {
-      // If 'expect' is 'true' then a pathValue should exist.
-      // If no value for expect then assume is a simple check to see it exists.
-      // Note: It's okay if the value is zero, or false but it should not be empty!
-      if (test.value !== 0 && test.value !== false && (!test.value || test.value.length === 0)) {
-        if (test.autoDetected) {
-          // Auto Detected properties by definition must exist, so if
-          // the test fail it can only be because they have no value.
-          testError = {
-            type: 'NULL',
-            message: `No value found for "${path}"`,
-          }
-        } else {
-          testError = {
-            type: 'NOT_FOUND',
-            message: `Could not find "${path}"`,
-          }
-        }
-      } else {
-        testPassed = true
-      }
-    } else if (test.expect === false) {
-      // If 'expect' is 'false' then a pathValue SHOULD NOT exist
-      if ((pathValue === null) || (test.type === 'metatag' && pathValue && pathValue[0] && typeof pathValue[0] === 'undefined')) {
-        testPassed = true
-      } else {
-        testError = {
-          type: 'PROPERTY_SHOULD_NOT_EXIST',
-          message: `Property "${path}" should not be defined`,
-        }
-      }
-    } else if (test.value && test.expect instanceof RegExp) {
-      // If test is a Regular Expression…
-      if (Array.isArray(test.value)) {
-        // If value is array and any key matches then test passes
-        test.value.map(value => {
-          if (value.match(test.expect))
-            testPassed = true
-        })
-      } else {
-        // If value is not an array, treat as a string
-        testPassed = Boolean(test.value.match(test.expect))
-      }
-
-      if (!testPassed) {
-        testError = {
-          type: 'INVALID_VALUE',
-          message: `Failed RegExp test for "${path}"`,
-          expected: test.expect,
-          found: test.value
-        }
-      }
-    } else if (!test.value || test.value.length === 0) {
-      // If item not found (or has no value) then error
-      testError = {
-        type: 'TEST_FAILED',
-        message: `Test "${path}" failed`,
-      }
-    } else if (test.value == test.expect || (Array.isArray(test.value) && test.value.includes(test.expect))) {
-      // If value is found and matches what we expect…
-      // …or in the case of metadata, if the returned value is an array 
-      // and one of the items matches what we expect then the test passes
-        testPassed = true
-    } else {
-      // If a value is found, but is does not match what we expect then fail
-      testError = {
-        type: 'INCORRECT_VALUE',
-        message: `Incorrect value found for "${path}"`,
-        expected: test.expect,
-        found: test.value,
-      }
-    }
-  } catch (e) {
-    testError = {
-      type: 'TEST_ERROR',
-      message: `Error evaluating test "${path}", check test syntax`,
-      e
-    }
-  }
-
-  return {
-    testPassed,
-    testError
-  }
-}
-
-const _runTest = (test, structuredData) => {
-  if (test.type == 'metatag') {
-    // Look for meta tags`
-    return _test(test, structuredData.metatags)
-  } else if (test.type == 'jsonld') {
-    // Look for data in jsonld
-    return _test(test, structuredData.jsonld)
-  } else if (test.type == 'microdata') {
-    // Look for data in microdata
-    return _test(test, structuredData.microdata)
-  } else if (test.type == 'rdfa') {
-    // Look for data in rdfa
-    return _test(test, structuredData.rdfa)
-  } else if (test.type == 'any' || !Object(test).hasOwnProperty('type')) {
-    let result = {}
-
-    // Look for data in jsonld, then microdata then rdfa then metatags until found
-    // If a test passes, set the test type to reflect where it was found
-    result = _test(test, structuredData.jsonld)
-    if (result.testPassed) {
-      test.type = 'jsonld'
-      return result
-    }
-
-    // If was not found in jsonld, look for data in microdata
-    result = _test(test, structuredData.microdata)
-    if (result.testPassed) {
-      test.type = 'microdata'
-      return result
-    }
-
-    // If was not found in jsonld or microdata, look for data in rdfa
-    result = _test(test, structuredData.rdfa)
-    if (result.testPassed) {
-      test.type = 'rdfa'
-      return result
-    }
-
-    // If was not found in jsonld or microdata or rdfa, look for data in metatags
-    result = _test(test, structuredData.metatags)
-    if (result.testPassed) {
-      test.type = 'metatag'
-      return result
-    }
-
-    // If property not found anywhere
-    return {
-      testPassed: false,
-      testError: {
-        type: 'NOT_FOUND',
-        message: `The property "${test.test}" was not found`,
-      }
-    }
-  } else {
-    return {
-      testPassed: false,
-      testError: {
-        type: 'INVALID_TYPE',
-        message: `The type "${test.type}" is invalid`,
-      }
-    }
-
-  }
-}
-
-const _findSchemas = (structuredData) => {
-  const schemas = []
-
-  const rdfa = Object.keys(structuredData.rdfa)
-  rdfa.map(schema => {
-    if (schema !== 'undefined' && !schemas.includes(schema)) schemas.push(`rdfa:${schema}`)
-  })
-
-  const microdata = Object.keys(structuredData.microdata)
-  microdata.map(schema => {
-    if (schema !== 'undefined' && !schemas.includes(schema)) schemas.push(`microdata:${schema}`)
-  })
-
-
-  const jsonld = Object.keys(structuredData.jsonld)
-  jsonld.map(schema => {
-    if (schema !== 'undefined' && !schemas.includes(schema)) schemas.push(`jsonld:${schema}`)
-  })
-
-  return schemas
 }
 
 const structuredDataTestUrl = async (url, options) => {
@@ -639,113 +453,6 @@ const __transformStructuredData = (structuredData) => {
     })
   })
   return result
-}
-
-const getTestsFromPreset = (preset, structuredData, testGroup) => {
-  const tests = []
-
-  if (!preset.tests)
-    return tests
-
-  const _setTestGroup = (test, preset) => {
-    // If test does not explicitly have a group defined, use the the default one preset,
-    // if defined. If no default group is is defined for the preset, fallback to using
-    // the preset name to group the results (every preset SHOULD have a name!)
-    if (!test.group) {
-      if (preset.group) {
-        test.group = preset.group
-      } else if (preset.name) {
-        test.group = preset.name
-      } else {
-        test.group = 'DEFAULT'
-      }
-    }
-  }
-
-  if (preset.schema) {
-    // If preset has a schema, return tests that check every instance of that schema
-    Object.keys(structuredData).map(dataType => {
-      if (dataType === 'metatags')
-        return
-      Object.keys(structuredData[dataType]).map((schemaName) => {
-        if (schemaName === preset.schema) {
-          structuredData[dataType][schemaName].forEach((instance, i) => {
-            const groups = testGroup.concat(`#${i} (${dataType})`)
-
-            preset.tests.forEach(t => {
-              // Note: Must copy object so we can re-use tests objects from a preset in different tests
-              const test = Object.assign({}, t)
-              test.schema = preset.schema
-              test.test = test.test.replace(/(.*)?\[\*\]/, `${preset.schema}[${i}]`)
-              test.type = dataType
-              test.groups = groups
-              if (!test.description)
-                test.description = test.test.replace(/(.*)?\[\d\]\./, '').replace(/"/g, '')
-
-              // If the test doesn't have an explict URL assigned to it but the preset does
-              // then assign the URL on the preset (which should point to documentation for
-              // preset) to the test, so it is easily acessible when displaying results.
-              if (!test.url && preset.url)
-                test.url = preset.url
-
-              tests.push(test)
-            })
-          })
-        }
-      })
-    })
-  } else {
-    // If preset does not have a schema, then return only the tests in the preset
-    preset.tests.forEach(test => {
-      _setTestGroup(test, preset)
-      // If the test doesn't have an explict URL assigned to it but the preset does
-      // then assign the URL on the preset (which should point to documentation for
-      // preset) to the test, so it is easily acessible when displaying results.
-      if (!test.url && preset.url)
-        test.url = preset.url
-      tests.push(test)
-    })
-  }
-
-  return tests
-}
-
-function _isSchemaOrgSchema(schema) {
-  if (schema['@context'] && schema['@context'].match(/^(http|https):\/\/schema.org/))
-    return true
-
-  if (schema['@type'] && schema['@type'].match(/^(http|https):\/\/schema.org/))
-    return true
-
-  return false
-}
-
-function _getSchemaTypeAndName(schema) {
-  let structuredDataType = null
-  let schemaName = schema
-
-  if (schemaName.match(/^jsonld:/))
-    [structuredDataType, schemaName] = [ 'jsonld', schema.replace(/^jsonld:/, '').replace(/^http:\/\/schema\.org\//, '') ]
-
-  if (schemaName.match(/^rdfa:/))
-    [structuredDataType, schemaName] = [ 'rdfa', schema.replace(/^rdfa:/, '').replace(/^http:\/\/schema\.org\//, '') ]
-
-  if (schemaName.match(/^microdata:/))
-    [structuredDataType, schemaName] = [ 'microdata', schema.replace(/^microdata:/, '').replace(/^http:\/\/schema\.org\//, '') ]
-
-  return [structuredDataType, schemaName]
-}
-
-// Add a test for any schema explicitly specified (or that was detected)
-const _addTestsForSchemaOrgSchema = (tests, name, groups, type, index) => {
-  tests.push({
-    test: `${name}[${(typeof(index) === 'undefined') ? '*': index}]`,
-    schema: name,
-    type: type || 'any',
-    group: name,
-    groups: groups,
-    description: (type) ? `Schema found in ${type}` : `Schema found`
-  })
 }
 
 module.exports = {
